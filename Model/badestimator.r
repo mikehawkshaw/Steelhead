078b6b25e5a90cd73c2cc2837cbaadbb783b0638
Brittany Jenewein <btjenewein@gmail.com>
2017-06-23 11:36:57 -0700

    Changed n_years to n_col because it makes more sense to me. Not actually the number of years in the dataset.

--- Model/badestimator.r
+++ Model/badestimator.r
@@ -5,10 +5,10 @@ setwd(data_dir)
 albion_annual<-read.table("steelhead_albion.csv",header=T, sep=",")
 
 
-n_years<-dim(albion_annual)[2]
+n_col<-dim(albion_annual)[2]
 
-m<-rep(0,n_years)
-s<-rep(0,n_years)
+m<-rep(0,n_col)
+s<-rep(0,n_col)
 
 deviates<-albion_annual-albion_annual
 
@@ -16,7 +16,7 @@ years<-seq(1995,2013)
 
 #ignore first colum because it has the day of year index and we don't want to fit a normal curve to that...
 
-for(i in 2:n_years)
+for(i in 2:n_col)
 {
 #normalize y to be a probability (sum = 1)
 p = albion_annual[,i]/sum(albion_annual[,i],na.rm=T)
@@ -41,28 +41,28 @@ lines(albion_annual[,1],pth, col="dark red",lwd=2)
 
 par(mfcol=c(1,3))
 
-m_mean = mean(m[2:n_years])
-m_sd = sd(m[2:n_years])
+m_mean = mean(m[2:n_col])
+m_sd = sd(m[2:n_col])
 m_aprox<-dnorm(min(albion_annual$jday):max(albion_annual$jday),m_mean,1.5*m_sd)
-#plot(density(m[2:n_years]))
-hist(m[2:n_years],prob=TRUE, breaks=20, main="",xlab="Distribution of means (N=20)")
+#plot(density(m[2:n_col]))
+hist(m[2:n_col],prob=TRUE, breaks=20, main="",xlab="Distribution of means (N=20)")
 lines(min(albion_annual$jday):max(albion_annual$jday),m_aprox,col="red")
 mtext(" (a) ",cex=0.75,line=-1.5,adj=1)
 
-s_mean = mean(s[2:n_years])
-s_sd = sd(s[2:n_years])
+s_mean = mean(s[2:n_col])
+s_sd = sd(s[2:n_col])
 s_aprox<-dnorm(5:50,s_mean,1.5*s_sd)
 #plot(density(s[2:53]))
-hist(s[2:n_years],prob=TRUE, breaks=20, main="",xlab="Distribution of standard deviations (N=20)")
+hist(s[2:n_col],prob=TRUE, breaks=20, main="",xlab="Distribution of standard deviations (N=20)")
 lines(5:50,s_aprox,col="red")
 mtext(" (b) ",cex=0.75,line=-1.5,adj=1)
 
-plot(m[2:n_years],s[2:n_years],xlab="mean",ylab="standard deviations")
+plot(m[2:n_col],s[2:n_col],xlab="mean",ylab="standard deviations")
 mtext(" (c) ",cex=0.75,line=-1.5,adj=1)
 
 par(mfcol=c(1,1))
 
-plot(years,m[2:n_years], xlab="Year", ylab="50% date", main="No Time Trend in Date of 50% return")
+plot(years,m[2:n_col], xlab="Year", ylab="50% date", main="No Time Trend in Date of 50% return")
 
 
 
