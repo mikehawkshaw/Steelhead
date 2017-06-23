d97662eca7a0da42a35a27d426f0b55a97a91a50
Brittany Jenewein <btjenewein@gmail.com>
2017-06-23 11:39:32 -0700

    Changed sequence generator for years to be dynamic. Easier to update when we get more data.

--- Model/badestimator.r
+++ Model/badestimator.r
@@ -12,7 +12,7 @@ s<-rep(0,n_col)
 
 deviates<-albion_annual-albion_annual
 
-years<-seq(1995,2013)
+years<-seq(1995,1995+n_col-2)
 
 #ignore first colum because it has the day of year index and we don't want to fit a normal curve to that...
 
