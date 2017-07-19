8559e47c8a6b2ad1a6b69e34865e0917ba72d927
Brittany Jenewein <btjenewein@gmail.com>
2017-07-19 09:23:49 -0700

    Fixed OpenBUGS model

--- Model/runtimingHBM_OpenBUGS.r
+++ Model/runtimingHBM_OpenBUGS.r
@@ -1,10 +1,13 @@
 #Model to estimate 50% run timing for steelhead over time series
 setwd("C:/DFO-MPO/github/Steelhead/Model")
 source("directories.R")
-library("xtable")
-library("reshape")
-library("stringr")
+library(xtable)
+library(reshape)
+library(stringr)
 library(R2OpenBUGS)
+library(mcmcplots)
+library(coda)
+
 setwd(data_dir)
 
 #Read in and set up data
@@ -40,7 +43,7 @@ mod_catch[,1]<-as.character(mod_catch[,1])
 
 for(i in 1:dim(mod_catch)[1]){
   
-  if(nchar(mod_catch[i,1]==2)){
+  if(nchar(mod_catch[i,1])==2){
   mod_catch[i,1]<-str_sub(mod_catch[i,1],-1)
   }
   mod_catch[i,1]<-str_sub(mod_catch[i,1],-2)
@@ -49,21 +52,40 @@ for(i in 1:dim(mod_catch)[1]){
 mod_catch[,1]<-as.numeric(mod_catch[,1])
 
 colnames(mod_catch)<-c("year","catch")
+mod_catch$day<- rep(seq(1,103,by=1),19)
 
 #Change back to model directory
 setwd(model_dir)
 
 #Prep inputs to model
-dat<-list(n_days=n_days, n_years=n_years, obs_abund=catch)
-inits<-list(list(mu_rt_m=290,sig_rt_m=20,mu_rt_sd=15,sig_rt_sd=8),list(mu_rt_m=280,sig_rt_m=22,mu_rt_sd=13,sig_rt_sd=7),list(mu_rt_m=300,sig_rt_m=10,mu_rt_sd=10,sig_rt_sd=9))
-parameters<-c("mu_rt_m","sig_rt_m","mu_rt_sd","sig_rt_sd","mu.year","sigma.year")
+dat<-list(n_days=n_days,n_years=n_years,obs_abund=catch)
+inits<-list(list(mu_rt_m=290,tau_rt_m=1,mu_rt_sd=15,tau_rt_sd=1,tau.c=1),list(mu_rt_m=280,tau_rt_m=1,mu_rt_sd=13,tau_rt_sd=1,tau.c=1),list(mu_rt_m=300,tau_rt_m=1,mu_rt_sd=10,tau_rt_sd=1,tau.c=1))
+parameters<-c("rt_m","rt_sd","mu_rt_m","sigma_rt_m","mu_rt_sd","sigma_rt_sd","sigma")
 
-RT_fit<- bugs(data=dat,inits=inits,parameters.to.save=parameters,model.file="runtimingHBM_OpenBUGSv4.txt",n.chains=3,n.iter=5000,debug=T)
-
-RT_fit$summary
+RT_fit<-bugs(data=dat,inits=inits,parameters.to.save=parameters,model.file="runtimingHBM_OpenBUGSv5.txt",n.chains=3,n.burnin=60000,n.iter=80000,debug=F)
 
 # Make results available so can call individual parameter sims (sims.list)
 attach.bugs(RT_fit)
+print(RT_fit)
+caterplot(RT_fit,parms=c("rt_m","mu_rt_m"),reorder=F)
+
+#Alternate option for getting trace plots into R
+RT_fit<-bugs(data=dat,inits=inits,parameters.to.save=parameters,model.file="runtimingHBM_OpenBUGSv5.txt",n.chains=3,n.iter=80000,debug=F,codaPkg=T)
+
+## Produce a CODA object from the lineout output
+line.coda <- read.bugs(RT_fit)
+## Summary statistics from the CODA output (uses coda package)
+summary(line.coda)
+## to change line.coda to pretend that only 1 chain
+line.coda2 <- as.mcmc(as.matrix(line.coda))
+#Summarize across chains
+HPDinterval(line.coda2)
+## Use CODA to produce diagnostic plots for the MCMC
+plot(line.coda)
+traceplot(line.coda)
+
+
+
 pth<-dnorm(albion_annual[,1],mean(mu),sd(sigma.year))
 par(mar=c(4,4,2,1))
 plot(albion_annual[,1],p,type="b", col="blue",main="50% estimate of steelhead run timing",xlab="Julian Date", ylab="Corrected Albion Catch",bty="l")
